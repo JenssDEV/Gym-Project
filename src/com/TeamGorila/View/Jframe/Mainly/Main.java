ed0f09791cdb2d503f8ad9f944d84a54743d4946
JensDev <jensluna89@gmail.com>
2024-06-27 21:26:10 -0500

    Parti 1 del menu

--- src/com/TeamGorila/View/Jframe/Mainly/Main.java
+++ src/com/TeamGorila/View/Jframe/Mainly/Main.java
@@ -1,18 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package com.TeamGorila.View.Jframe.Mainly;
 
-/**
- *
- * @author JENS07
- */
 public class Main extends javax.swing.JFrame {
 
-    /**
-     * Creates new form Main
-     */
     public Main() {
         initComponents();
     }
---
+++ src/com/TeamGorila/View/Jframe/Mainly/theme/SystemTheme.java
@@ -0,0 +1,8 @@
+package com.TeamGorila.View.Jframe.Mainly.theme;
+
+import java.awt.Color;
+
+public class SystemTheme {
+
+    public static Color mainColor = new Color(54, 149, 255);
+}
